ee500baef300acc9fb196fece2b8b8b8de0fccc2
Ekaterina Tochilina <katerina_t_n@mail.ru>
2023-04-12 16:31:10 +0300

    removed extra code

--- Python/ceval.c
+++ Python/ceval.c
@@ -23,7 +23,6 @@
 #include "pycore_sysmodule.h"     // _PySys_Audit()
 #include "pycore_tuple.h"         // _PyTuple_ITEMS()
 #include "pycore_emscripten_signal.h"  // _Py_CHECK_EMSCRIPTEN_SIGNALS
-#include "pycore_frame.h"
 
 #include "pycore_dict.h"
 #include "dictobject.h"
@@ -37,7 +36,6 @@
 
 #include <ctype.h>
 #include <stdbool.h>
-#include <pthread.h>
 
 #ifdef Py_DEBUG
    /* For debugging the interpreter: */
